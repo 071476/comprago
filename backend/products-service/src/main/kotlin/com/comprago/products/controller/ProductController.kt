03069d15deb12c6801e69d92490742bc7f7368e6
Ali García Fernández <ali-gafer@hotmail.com>
2026-08-14 18:28:35 -0500

    fix: @CrossOrigin en ProductController

--- backend/products-service/src/main/kotlin/com/comprago/products/controller/ProductController.kt
+++ backend/products-service/src/main/kotlin/com/comprago/products/controller/ProductController.kt
@@ -8,6 +8,7 @@ import org.springframework.web.bind.annotation.*
 
 @RestController
 @RequestMapping("/api/products")
+@CrossOrigin(origins = ["*"], methods = [RequestMethod.GET, RequestMethod.POST, RequestMethod.PUT, RequestMethod.DELETE, RequestMethod.OPTIONS])
 class ProductController(private val productService: ProductService) {
 
     @PostMapping
